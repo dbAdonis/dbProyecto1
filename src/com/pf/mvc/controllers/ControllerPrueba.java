ea18d39471fb8354d99b7e69e98fc0d528190b31
User <User@DESKTOP-QFHL8EG>
2024-11-01 17:20:02 -0600

    Vamos a cagar

--- src/com/pf/mvc/controllers/ControllerPrueba.java
+++ src/com/pf/mvc/controllers/ControllerPrueba.java
@@ -3,7 +3,7 @@ package com.pf.mvc.controllers;
 public class ControllerPrueba {
 
 	public ControllerPrueba() {
-		// SI LES SALE ESTO ES PORQUE NO CAGAMOS :)
+		// SI LES SALE ESTO ES PORQUE NO CAGAMOS
 	}
 
 }

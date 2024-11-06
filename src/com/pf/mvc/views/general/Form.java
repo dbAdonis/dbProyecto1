b86efbe37125cfd624bad27d82132ebc788b78f8
User <User@DESKTOP-QFHL8EG>
2024-11-06 16:15:34 -0600

    Cagamos

---
+++ src/com/pf/mvc/views/general/Form.java
@@ -0,0 +1,14 @@
+package com.pf.mvc.views.general;
+
+import javax.swing.JPanel;
+
+public class Form extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Form() {
+
+	}
+
+}
---
+++ src/com/pf/mvc/views/general/Index.java
@@ -0,0 +1,14 @@
+package com.pf.mvc.views.general;
+
+import javax.swing.JPanel;
+
+public class Index extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Index() {
+
+	}
+
+}
